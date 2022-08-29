415b6b88caf976cd6a7635540f99fb765752837e
Samuel <slopeza@uninorte.edu.co>
2022-08-29 16:18:00 -0500

    Changes before explosion

---
+++ hooks/useFetch.js
@@ -0,0 +1,64 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-tabs */
+/* eslint-disable no-unused-vars */
+import { useCallback, useEffect, useState } from 'react'
+
+export const useFetch = ({
+    service,
+    init
+}) => {
+    const [data, setData] = useState<T['result'] | undefined>(undefined)
+    const [loading, setLoading] = useState(false)
+    const [called, setCalled] = useState(false)
+    const [error, setError] = useState<TError | undefined>(undefined)
+    /**
+     * Función que ejecuta el servicio y captura los estados de la petición
+     * @param {T} args argumentos del servicio
+     * @returns {void}
+     */
+    const getService = async (...args) => {
+        try {
+            setLoading(true)
+            const res = await service(...args)
+            setLoading(false)
+            setCalled(true)
+            setError(undefined)
+            setData(res)
+            return res
+        } catch (err) {
+            setLoading(false)
+            setData(undefined)
+            setError({
+                message:
+                    err?.response?.data?.message ||
+                    err?.response?.data?.error ||
+                    err?.message ||
+                    'Se ha producido un error',
+                code: err?.response?.status || err?.code || 404
+            })
+        }
+    }
+    /**
+     * Función que reinicia los estados
+     * @returns {void}
+     */
+    const reset = useCallback(() => {
+        setData(undefined)
+        setLoading(false)
+        setCalled(false)
+        setError(undefined)
+    }, [])
+    // Efecto que ejecuta el servicio cuando inicializa el componente
+    useEffect(() => {
+        init && getService()
+    }, [])
+    return {
+        data,
+        loading,
+        called,
+        error,
+        fetch: getService,
+        reset,
+        setForcedData: setData
+    }
+}
