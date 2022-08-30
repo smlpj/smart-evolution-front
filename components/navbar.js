387a4a49d3333b32ccbeeefc7a79aeac6e1aa09e
smlpj <slopeza@uninorte.edu.co>
2022-08-30 16:11:58 -0500

    Navbar updated

--- components/navbar.js
+++ components/navbar.js
@@ -2,25 +2,62 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import NavbarButton from "../styles/button_2";
 
-
 export default function Navbar() {
     return (
         <>
             <Grid container spacing={0}>
-                <Grid item xs={12}
-                    sx={{ backgroundColor: '#EBEBEB', height: '100vh', color: 'black' }}
+                <Grid
+                    item
+                    xs={12}
+                    sx={{ backgroundColor: "#EBEBEB", height: "100vh", color: "black" }}
                     display="flex"
                     alignItems="center"
-                    justifyContent="center">
-                    <Box display="flex" flexDirection="column" sx={{ borderRadius: '4px', border: '1.4px solid', borderColor: '#B5D1C9', padding: '20px' }}>
-                        <NavbarButton>Clientes</NavbarButton>
-                        <NavbarButton>Pre-Operaciones</NavbarButton>
-                        <NavbarButton>Operaciones</NavbarButton>
-                        <NavbarButton>Corredores</NavbarButton>
-                        <NavbarButton>Administración</NavbarButton>
+                    justifyContent="center"
+                >
+                    <Box display="flex" flexDirection="column">
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            sx={{
+                                borderRadius: "4px 4px 0px 0px",
+                                border: "2px solid",
+                                borderColor: "#B5D1C9",
+                                padding: "70px",
+                                borderBottom: 0
+                            }}
+                        >
+                            <NavbarButton sx={{ marginBottom: "25px" }}>
+                                Clientes
+                            </NavbarButton>
+                            <NavbarButton sx={{ marginBottom: "25px" }}>
+                                Pre-Operaciones
+                            </NavbarButton>
+                            <NavbarButton sx={{ marginBottom: "25px" }}>
+                                Operaciones
+                            </NavbarButton>
+                            <NavbarButton sx={{ marginBottom: "25px" }}>
+                                Corredores
+                            </NavbarButton>
+                            <NavbarButton>Administración</NavbarButton>
+                        </Box>
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            sx={{
+                                borderRadius: "0px 0px 4px 4px",
+                                border: "2px solid",
+                                borderColor: "#B5D1C9",
+                                backgroundColor: "#FAFAFA",
+                                padding: "150px 60px 150px 60px",
+                            }}
+                        >
+                            <NavbarButton sx={{ backgroundColor: "#FFFFFF" }}>
+                                Inversores disponibles
+                            </NavbarButton>
+                        </Box>
                     </Box>
                 </Grid>
             </Grid>
         </>
-    )
+    );
 }
