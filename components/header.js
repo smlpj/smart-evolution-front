e6b535075a67c6f35dc4b8958e6746d134b32765
smlpj <slopeza@uninorte.edu.co>
2022-08-31 12:25:31 -0500

    Rip

--- components/header.js
+++ components/header.js
@@ -10,37 +10,68 @@ import HeaderButton from "../styles/button_2";
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: "#EBEBEB",
-          borderBottom: "1.4px solid #5EA3A380",
-          padding: "0.5vh 5vw 0.5vh 5vw",
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <img
-              style={{ height: "3.4vh" }}
-              src="/assets/Icono Smart.svg"
-            ></img>
-          </IconButton>
-          <HeaderButton
-            sx={{ border: "1.4px solid #5EA3A3", padding: "1vh 1vw 1vh 1vw" }}
-            endIcon={<img src="/assets/Icon Home.svg"></img>}
-          >
-            Inicio
-          </HeaderButton>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ width: "100vw" }}>
+        <AppBar
+          position="static"
+          sx={{
+            backgroundColor: "#EBEBEB",
+            borderBottom: "1.4px solid #5EA3A380",
+            padding: "0.5vh 5vw 0.5vh 5vw",
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <img
+                style={{ height: "3.4vh" }}
+                src="/assets/Icono Smart.svg"
+              ></img>
+            </IconButton>
+            <HeaderButton
+              sx={{
+                border: "1.4px solid #5EA3A3",
+                padding: "1vh 1vw 1vh 1vw",
+              }}
+              endIcon={
+                <img
+                  style={{ marginLeft: "2vh" }}
+                  src="/assets/Icon Home.svg"
+                />
+              }
+            >
+              Inicio
+            </HeaderButton>
+            <IconButton
+              size="large"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+            >
+              <HeaderButton
+                sx={{
+                  border: "1.4px solid #5EA3A3",
+                  borderRadius: "50%",
+                  padding: "1vh 1vw 1vh 1vw",
+                }}
+              >
+                <img
+                  style={{ height: "2vh" }}
+                  src="/assets/Icon - Notificación.svg"
+                ></img>
+              </HeaderButton>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 }
