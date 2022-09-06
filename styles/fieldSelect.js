32231a42ab8734dd48ad790394e26f0d525563b3
smlpj <slopeza@uninorte.edu.co>
2022-09-06 15:12:31 -0500

    Dashboard finished (first version) and register

---
+++ styles/fieldSelect.js
@@ -0,0 +1,15 @@
+import { Autocomplete } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const SelectField = styled(Autocomplete)({
+  backgroundColor: "white",
+  border: "1px solid #ACCFCF",
+  borderRadius: "5px",
+  padding: "10px",
+  "& .MuiInputBase-input": {
+    padding: "2px",
+    fontFamily: "Montserrat",
+  },
+});
+
+export default SelectField;
