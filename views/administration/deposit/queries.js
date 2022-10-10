b41e7a3f6c2eb22794046b6dda1b1c8f95a55935
smlpj <slopeza@uninorte.edu.co>
2022-10-10 10:48:31 -0500

    Creating administration and deposit module

---
+++ views/administration/deposit/queries.js
@@ -0,0 +1,42 @@
+import Axios from "axios";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3Nzg4MDg5LCJpYXQiOjE2NjUxNjAwODksImp0aSI6IjE5N2E1MDVjYWU0MDQyNGNiOTlhY2YyYWFkOTliYWI4IiwidXNlcl9pZCI6IjFiNzZkNTEwLTBjODgtNDY4Yi05YzE1LWNiMTdhMjZlODM0MSIsIm5hbWUiOiJwcm9kdWN0aW9uIHVzZXIiLCJyb2xlcyI6WyJzdXBlcnVzZXIiXSwiaXNfc3VwZXJ1c2VyIjp0cnVlfQ.7e1R6qx1nRcVbrYgxPPnlTpYx35nvN-fP_5GOgu2DtU";
+
+export const RegisterBrokerQuery = async (data) => {
+  const res = await Axios.post(
+    "https://smart-evolution-api2.herokuapp.com/api/broker/",
+    data,
+    {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    }
+  );
+  return res.data;
+};
+
+export const GetBrokerByID = async (id) => {
+  const res = await Axios.get(
+    `https://smart-evolution-api2.herokuapp.com/api/broker/${id}`,
+    {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    }
+  );
+  return res.data;
+};
+
+export const ModifyBrokerQuery = async (data) => {
+  console.log(data);
+  const res = await Axios.patch(
+    `https://smart-evolution-api2.herokuapp.com/api/broker/${data.id}`,
+    data,
+    {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    }
+  );
+  return res.data;
+};
