7de647884570e487736e5a9ef2410df9fd871ad6
smlpj <slopeza@uninorte.edu.co>
2022-10-20 16:04:12 -0500

    Fontawesome added

--- pages/_app.js
+++ pages/_app.js
@@ -3,28 +3,29 @@ import { useRouter } from "next/router";
 import { Grid } from "@mui/material";
 import Layout from "../shared/components/layout";
 import Head from "next/head";
-import 'react-toastify/dist/ReactToastify.css';
-import { AuthProvider } from '../shared/context/authContext'
-import '../public/icomoon/style.css'
+import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "../shared/context/authContext";
+import "../public/icomoon/style.css";
+import "../public/fontawesome/css/all.min.css";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <>
-    <AuthProvider>
-      {router.pathname !== "/auth/login" &&
-      router.pathname  !== "/brokers" &&
-      router.pathname  !== "/customers" &&
-      router.pathname  !== "/" &&
-      router.pathname  !== "/administration/deposit-investor" && 
-      router.pathname  !== "/administration/deposit-emitter" ? (
-        <Layout>
+      <AuthProvider>
+        {router.pathname !== "/auth/login" &&
+        router.pathname !== "/brokers" &&
+        router.pathname !== "/customers" &&
+        router.pathname !== "/" &&
+        router.pathname !== "/administration/deposit-investor" &&
+        router.pathname !== "/administration/deposit-emitter" ? (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        ) : (
           <Component {...pageProps} />
-        </Layout>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </AuthProvider>
+        )}
+      </AuthProvider>
     </>
   );
 }
