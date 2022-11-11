309a8ccd225c82e995d4a1cb2594afe0aaab749b
inf-anderson <anderson.sanchez@infinitytech.com.co>
2022-11-11 16:23:18 -0500

    migrate from heroku to railway

--- views/administration/refund/refundList/index.js
+++ views/administration/refund/refundList/index.js
@@ -1,42 +1,66 @@
 // React
-import { useEffect, useState } from "react"
-// Hooks
-import { useFetch } from "@hooks/useFetch"
-// Components
-import { RefundListC } from './components'
-// Queries
-import { GetRefund } from './queries'
+import { useEffect, useState } from "react";
 
+// Hooks
+import { useFetch } from "@hooks/useFetch";
+
+// Components
+import { RefundListC } from "./components";
+// Queries
+import { GetRefund } from "./queries";
 
 export const RefundListV = () => {
-    
-    // State
-    const [refund, setRefund] = useState([])
+  // State
+  const [refund, setRefund] = useState([]);
+  const [nextPage, setNextPage] = useState(null);
+  const [previousPage, setPreviousPage] = useState(null);
+  const [currentPage, setCurrentPage] = useState(
+    "https://smart-evolution-api2.herokuapp.com/api/refund/"
+  );
 
-    // Queries
-    const {
-      fetch: fetch,
-      loading: loading,
-      error: error,
-      data: data,
-    } = useFetch({ service: GetRefund, init: true })
+  // Queries
+  const {
+    fetch: fetch,
+    loading: loading,
+    error: error,
+    data: data,
+  } = useFetch({ service: GetRefund, init: false });
 
-    useEffect(() => {
-        if(data) {
-            const parsed = data.data.map((item) => {
-                return {
-                    ...item,
-                    account:item?.account?.account_number,
-                    accountType:item?.accountType?.description,
-                    bank:item?.bank?.description,
-                    client:item?.client?.social_reason ? item?.client?.social_reason : `${item?.client?.first_name} ${item.client.last_name}`,
-                }
-            })
-            setRefund(parsed)
-        }
-    },[loading, error, data])
+  useEffect(() => {
+    fetch(currentPage);
+  }, []);
 
-    return (
-        <RefundListC data={refund} />
-    )
-}
+  useEffect(() => {
+    if (data) {
+      const parsed = data.results.map((item) => {
+        return {
+          ...item,
+          account: item?.account?.account_number,
+          accountType: item?.accountType?.description,
+          bank: item?.bank?.description,
+          client: item?.client?.social_reason
+            ? item?.client?.social_reason
+            : `${item?.client?.first_name} ${item.client.last_name}`,
+        };
+      });
+      setRefund(parsed);
+      setNextPage(data.next);
+      setPreviousPage(data.previous);
+    }
+  }, [loading, error, data]);
+
+  const getRefundData = (page, action) => {
+    if (action === "next" && nextPage) fetch(nextPage)
+    else if (action === 'previous' && previousPage) fetch(previousPage)
+  }
+
+  return (
+    <RefundListC
+      data={refund}
+      nextPage={nextPage}
+      previousPage={previousPage}
+      getRefundData={getRefundData}
+      loading={loading}
+    />
+  );
+};
--- views/administration/refund/refundList/queries.js
+++ views/administration/refund/refundList/queries.js
@@ -2,11 +2,11 @@ import Axios from 'axios'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-export const GetRefund = async () => {
-    const res = await Axios.get(`${API_URL}/refund/`, {
+export const GetRefund = async (url) => {
+    const res = await Axios.get(`${url}`, {
       headers: {
         authorization: "Bearer " + localStorage.getItem("access-token"),
       },
     });
     return res.data;
-  };
+}
